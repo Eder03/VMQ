91be4ef2b801163b07d6f480874fa5b915eb86d9
alexeder03 <alexander.eder2003@gmail.com>
2024-07-02 17:06:44 +0200

    AddSong rework

--- src/Sites/AddSong.js
+++ src/Sites/AddSong.js
@@ -1,202 +1,227 @@
-import React, { Component } from 'react'
-import { Form, GridRow, Label, Grid, Dropdown, Table, Button } from 'semantic-ui-react'
+import React, { Component } from 'react';
+import { Form, Label, Grid, Table, Button, Container, Header, Segment, Message, Icon } from 'semantic-ui-react';
 import axios from 'axios';
-
-
+import YouTube from 'react-youtube';
 
 export default class AddSong extends Component {
   constructor(props) {
-      super(props)
+    super(props);
 
-      //Initialisierung der Funktionen um den Input zu akzeptieren
-      this.onChangeSongName = this.onChangeSongName.bind(this);
-      this.onChangeLink = this.onChangeLink.bind(this);
-      this.onChangeComposers = this.onChangeComposers.bind(this);
-      this.onChangeDropdown = this.onChangeDropdown.bind(this);
-      this.onSubmit = this.onSubmit.bind(this);
+    this.onChangeSongName = this.onChangeSongName.bind(this);
+    this.onChangeLink = this.onChangeLink.bind(this);
+    this.onChangeComposers = this.onChangeComposers.bind(this);
+    this.onChangeDropdown = this.onChangeDropdown.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.extractYoutubeId = this.extractYoutubeId.bind(this);
+    this.playSong = this.playSong.bind(this);
+    this.stopSong = this.stopSong.bind(this);
 
-      //Deklaration der einzelnen Datenbank Variablen und anderer Variablen
-      this.state = {
-          musicData: [],
-          //Daten von der Datenbank werden in ein eigenes Objekt für das Dropdown gespeichert
-          dropdownData: [],
-          //Standard Form, damit das Dropdown es erkennt
-          dropdownOptions:[],
-          game: '',
-          songname: '',
-          link:'',
-          composers:'',
-
-          songs: [
-         ],
-         video: ""
-
-      }
-
-
-//Einzelne Set Funktionen
+    this.state = {
+      musicData: [],
+      dropdownData: [],
+      dropdownOptions: [],
+      game: '',
+      songname: '',
+      link: '',
+      youtubeId: '',
+      composers: '',
+      songs: [],
+      loading: false,
+      successMessage: '',
+      errorMessage: '',
+      currentPlayingSong: null,
+      isPlaying: false,
+    };
   }
-  
+
   onChangeSongName(e) {
-    this.setState({ songname: e.target.value })
-}
+    this.setState({ songname: e.target.value });
+  }
 
-onChangeLink(e) {
-  this.setState({ link: e.target.value })
-}
+  onChangeLink(e) {
+    const url = e.target.value;
+    const youtubeId = this.extractYoutubeId(url);
+    this.setState({ link: url, youtubeId });
+  }
 
-onChangeComposers(e){
-  this.setState({composers: e.target.value})
-}
+  onChangeComposers(e) {
+    this.setState({ composers: e.target.value });
+  }
 
+  onChangeDropdown(e, { value }) {
+    const game = value;
+    const selectedGame = this.state.musicData.find(data => data.game === game);
 
-//Funktion um die Tabelle zu aktualisieren, damit die richtigen Songs angezeigt werden
-onChangeDropdown(e) {
+    if (selectedGame) {
+      const newSongs = selectedGame.songs.map((song, index) => ({
+        id: index + 1,
+        nr: index + 1,
+        songname: song.name,
+        link: song.link,
+        approved: song.songapproved ? 'true' : 'false',
+      }));
 
-  this.setState((currentState) =>{
-    //Songs die angezeigt werden sollen
-  const newSongs = [];
-  //Spiel das ausgewählt wird
-  const game = e.target.textContent;
-
-  //For Schleife, die für jedes vorhandene Spiel Objekt ausgeführt wird, bis das ausgewählte gefunden wurde
-  this.state.musicData.forEach((data) =>{
-    if(data.game == game){
-      //For Schleife, die für jedes vorhandene Musik Objekt im ausgewählten Spiel ausgeführt wird
-         data.songs.forEach((song, index) =>{
-        newSongs.push({
-          id: index+1,
-          nr: index+1,
-          songname: song.name,
-          link: song.link,
-          approved: song.songapproved
-        })
-      })
+      this.setState({ game, songs: newSongs });
     }
-  })
-  return{
-    //Um die Alten Daten zu behalten, falls bei der Auswahl eines neuen Spiels ein Fehler aufgetreten ist
-    ...currentState,
-    game,
-    songs: newSongs,
   }
-})
-}
 
+  extractYoutubeId(url) {
+    const regex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
+    const matches = url.match(regex);
+    return matches ? matches[1] : '';
+  }
 
-//Standard Funktion; wird aufgerufen bevor/während die Seite geladen wird
-  componentDidMount(){
-    //Get Request für alle Daten
+  componentDidMount() {
     axios.get('https://vmq.onrender.com/getAll')
-    .then(res => {
-      //Alle Daten werden in ein Objekt abgespeichert
-        this.setState({ musicData: res.data });
-        for(var i = 0; i < this.state.musicData.length; i++){
-          //Namen der Spiele wird in DropdownData gespeichert
-          this.state.dropdownData.push(this.state.musicData[i].game)
+      .then(res => {
+        const dropdownOptions = res.data.map(game => ({
+          key: game.game,
+          text: game.game,
+          value: game.game,
+        }));
 
-        }
-        
-        //DropdownOptions wird in die richtige Form gebracht und alle Daten hineingegeben
-        for(var j = 0; j < this.state.dropdownData.length; j++){
-          this.state.dropdownOptions.push({key: this.state.dropdownData[j], text: this.state.dropdownData[j], value: this.state.dropdownData[j]})
-        }
-
-       
-    })
-    .catch(function (error) {
+        this.setState({ musicData: res.data, dropdownOptions });
+      })
+      .catch(error => {
         console.log(error);
-    })
-
+      });
   }
 
-  
-
-  
-
-
-  //Daten der Tabelle wird für das richtige Spiel gerendert
   renderTableData() {
-    return this.state.songs.map((song, index) => {
-       var { id, nr, songname, link, approved } = song 
-       if(!approved){
-        approved = "false"
-      }else{
-        approved = "true"
-      }
-       return (
-          <Table.Row key={id}>
-             <Table.Cell>{nr}</Table.Cell>
-             <Table.Cell>{songname}</Table.Cell>
-             <Table.Cell>{link}</Table.Cell>
-             <Table.Cell>{approved}</Table.Cell>
-          </Table.Row>
-       )
-    })
- }
+    return this.state.songs.map(song => (
+      <Table.Row key={song.id}>
+        <Table.Cell>{song.nr}</Table.Cell>
+        <Table.Cell>{song.songname}</Table.Cell>
+        <Table.Cell>{song.link}</Table.Cell>
+        <Table.Cell>{song.approved}</Table.Cell>
+        <Table.Cell style={{ width: '120px', textAlign: 'center' }}>
+          <Button.Group>
+            <Button icon onClick={() => this.playSong(song.link)} disabled={this.state.currentPlayingSong === song.link}>
+              <Icon name='play' />
+            </Button>
+            {this.state.currentPlayingSong === song.link && (
+              <Button icon onClick={this.stopSong}>
+                <Icon name='stop' />
+              </Button>
+            )}
+          </Button.Group>
+        </Table.Cell>
+      </Table.Row>
+    ));
+  }
 
-  onSubmit(e){
+  playSong(link) {
+    this.setState({ currentPlayingSong: link, isPlaying: true });
+  }
+
+  stopSong() {
+    this.setState({ currentPlayingSong: null, isPlaying: false });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    this.setState({ loading: true });
 
     const songObject = {
       game: this.state.game,
-      songs: 
-        {
-          name: this.state.songname,
-          link: this.state.link,
-          composers: this.state.composers.split(", "),
-          songapproved: false
-        }
-      
-  };
-  //Update Endpoint wird aufgerufen und die eingegebenen Daten übergeben
-  axios.post('https://vmq.onrender.com/update', songObject)
-          .then((res) => {
-              console.log(res.data)
-          }).catch((error) => {
-              console.log(error)
-          });
+      songs: {
+        name: this.state.songname,
+        link: this.state.youtubeId,
+        composers: this.state.composers.split(', '),
+        songapproved: false,
+      },
+    };
 
-    this.setState({songname:'', link:'', composers:''})
+    axios.post('https://vmq.onrender.com/update', songObject)
+      .then(res => {
+        console.log(res.data);
+
+        const newSong = {
+          id: this.state.songs.length + 1,
+          nr: this.state.songs.length + 1,
+          songname: this.state.songname,
+          link: this.state.youtubeId,
+          approved: 'false',
+        };
+
+        this.setState(prevState => ({
+          successMessage: 'Song successfully added!',
+          errorMessage: '',
+          loading: false,
+          songname: '',
+          link: '',
+          youtubeId: '',
+          composers: '',
+          songs: [...prevState.songs, newSong],
+        }));
+      })
+      .catch(error => {
+        console.log(error);
+        this.setState({
+          errorMessage: 'An error occurred. Please try again later.',
+          successMessage: '',
+          loading: false,
+        });
+      });
   }
 
-
-
   render() {
-    
-      return (
-        <div>
-          {/* Abstand zu Navbar */}
-          <br></br>
+    const { game, songname, link, youtubeId, composers, loading, successMessage, errorMessage, currentPlayingSong, isPlaying } = this.state;
 
-          <Form>
-            <Form.Dropdown 
-            placeholder='Select Game'
-            fluid
-            selection
-            options={this.state.dropdownOptions}
-            onChange={this.onChangeDropdown}
-            >
-            
-            </Form.Dropdown>
+    const videoOptions = {
+      height: '0',
+      width: '0',
+      playerVars: {
+        autoplay: 1,
+      },
+    };
 
-            <Label>Song Information</Label>
-            <Form.Group>
-            <Form.Input width="4"
+    return (
+      <Container>
+        <Segment padded='very'>
+          <Header as='h2' textAlign='center' color='teal'>Add New Song to Existing Game</Header>
+          <Form onSubmit={this.onSubmit} loading={loading} error={!!errorMessage} success={!!successMessage}>
+            <Form.Dropdown
+              placeholder='Select Game'
+              fluid
+              selection
+              options={this.state.dropdownOptions}
+              onChange={this.onChangeDropdown}
+              value={game}
+              required
+            />
+
+<Label style={{ marginBottom: '10px' }}>Song Information</Label>
+            <Form.Group widths='equal'>
+              <Form.Input
+                label={<label>Songname</label>}
                 placeholder='Songname e.g. Main Theme'
                 name='songName'
-                value={this.state.songname}
+                value={songname}
                 onChange={this.onChangeSongName}
+                required
               />
-              <Form.Input width="3"
-                placeholder='Youtube ID e.g. UC_U0l2E-Rs'
+              <Form.Input
+                label={<label>YouTube URL</label>}
+                placeholder='YouTube URL e.g. https://youtu.be/UC_U0l2E-Rs'
                 name='link'
-                value={this.state.link}
+                value={link}
                 onChange={this.onChangeLink}
+               
               />
-              <Form.Input width="4"
+              <Form.Input
+                label={<label>YouTube ID</label>}
+                placeholder='YouTube ID'
+                name='youtubeId'
+                value={youtubeId}
+                readOnly
+                required
+              />
+              <Form.Input
+                label='Composers'
                 placeholder='Composers e.g. Koji Kondo, Toby Fox'
                 name='composers'
-                value={this.state.composers}
+                value={composers}
                 onChange={this.onChangeComposers}
               />
             </Form.Group>
@@ -204,26 +229,35 @@ onChangeDropdown(e) {
             <Grid>
               <Grid.Row>
                 <Grid.Column>
-                <Form.Button onClick={this.onSubmit} content='Submit' color='green'/>
+                  <Form.Button content='Submit' color='green' />
                 </Grid.Column>
-                
               </Grid.Row>
             </Grid>
+            <Message success header='Success' content={successMessage} />
+            <Message error header='Error' content={errorMessage} />
           </Form>
 
-          <Table>
-           <Table.Header>
-               <Table.Row>
-                  <Table.HeaderCell width={1}>Nr</Table.HeaderCell>
-                  <Table.HeaderCell width={2}>Songname</Table.HeaderCell>
-                  <Table.HeaderCell width={1}>Link</Table.HeaderCell>
-                  <Table.HeaderCell width={1}>approved</Table.HeaderCell>
-               </Table.Row>
-           </Table.Header>
-           {this.renderTableData()}
-        </Table>
+          <Table celled>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell>Nr</Table.HeaderCell>
+                <Table.HeaderCell>Songname</Table.HeaderCell>
+                <Table.HeaderCell>Link</Table.HeaderCell>
+                <Table.HeaderCell>Approved</Table.HeaderCell>
+                <Table.HeaderCell>Actions</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+              {this.renderTableData()}
+            </Table.Body>
+          </Table>
+
           
-        </div>
-      )
+        </Segment>
+        {isPlaying && (
+            <YouTube videoId={currentPlayingSong} opts={videoOptions} onEnd={this.stopSong} />
+          )}
+      </Container>
+    );
   }
 }
